7ee6f0d8e05071f2b48cabb1268a557f44b8043f
Kishore Balaji P <kishorebalaji880@gmail.com>
2026-02-21 18:09:05 +0530

    vercel deploy changes

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// test route 
+app.get("/", (req, res) => {
+  res.send("Server running successfully");
+});
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+module.exports = app;
